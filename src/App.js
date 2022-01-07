9f2cd30e9693c5c6aa8417fae9b4cb1470eeaacc
Shorya-agarwal <91627681+Shorya-agarwal@users.noreply.github.com>
2022-01-07 17:31:33 +0530

    Added .env.local file

--- src/App.js
+++ src/App.js
@@ -9,20 +9,21 @@ import {
  } from "react-router-dom";
 
 export default class App extends Component {
+  apiKey = process.env.REACT_APP_NEWS_API
   render() {
     return (
       <div>
         <Router>
           <Navbar />
           <Switch>
-            <Route exact path="/"><News pageSize={9} category="general" key="general1" /></Route>
-            <Route exact path="/general"><News pageSize={9} category="general" key="general" /></Route>
-            <Route exact path="/business"><News pageSize={9} category="business" key="business" /></Route>
-            <Route exact path="/entertainment"><News pageSize={9}  key="entertainment" category="entertainment" /></Route>
-            <Route exact path="/health"><News pageSize={9}   key="health" category="health" /></Route>
-            <Route exact path="/science"><News pageSize={9}  key="science" category="science" /></Route>
-            <Route exact path="/sports"><News pageSize={9}  key="sports" category="sports" /></Route>
-            <Route exact path="/technology"><News pageSize={9}  key="technology" category="technology" /></Route>
+            <Route exact path="/"><News pageSize={9} apiKey={this.apiKey} category="general" key="general1" /></Route>
+            <Route exact path="/general"><News pageSize={9} apiKey={this.apiKey} category="general" key="general" /></Route>
+            <Route exact path="/business"><News pageSize={9} apiKey={this.apiKey} category="business" key="business" /></Route>
+            <Route exact path="/entertainment"><News pageSize={9} apiKey={this.apiKey}  key="entertainment" category="entertainment" /></Route>
+            <Route exact path="/health"><News pageSize={9} apiKey={this.apiKey}   key="health" category="health" /></Route>
+            <Route exact path="/science"><News pageSize={9} apiKey={this.apiKey}  key="science" category="science" /></Route>
+            <Route exact path="/sports"><News pageSize={9} apiKey={this.apiKey}  key="sports" category="sports" /></Route>
+            <Route exact path="/technology"><News pageSize={9} apiKey={this.apiKey}  key="technology" category="technology" /></Route>
           </Switch>
         </Router>
       </div>
