ddb3e72dc171a6aab624d1c773f2dfe65c19789c
Shorya-agarwal <91627681+Shorya-agarwal@users.noreply.github.com>
2022-02-17 09:32:08 +0530

    Fixes some bugs and finalize it completely

--- src/components/News.js
+++ src/components/News.js
@@ -64,7 +64,7 @@ export class News extends Component {
     render() {
         return (
             <>
-                <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
+                <h1 className="text-center" style={{ margin: '75px 0px 35px 0px' }}>NewsNation - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
                 {this.state.loading && <Spinner />}
                 <InfiniteScroll
                     dataLength={this.state.articles.length}
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,11 +5,11 @@ export class NewsItem extends Component {
         let { title, description, imgUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className="my-3">
-                <div className="card">
-                    <div style={{display: 'flex',justifyContent: 'flex-end',position: 'absolute',right: '0'}}>
+                <div className="card" >
+                    <div style={{display: 'flex',justifyContent: 'flex-end',position: 'absolute',left: '90%'}}>
                         <span className="badge rounded-pill bg-success"> {source} </span>
                     </div>
-                    <img src= {!imgUrl?"https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/yRF5c-O/breaking-news-on-screen-animated-text-graphics-news-broadcast-graphic-title-animation-loop-full-hd-1920x1080-blue-cyan_hkrmjv15e_thumbnail-1080_05.png" :imgUrl} className="card-img-top" alt="..."/>
+                    <img src= {!imgUrl?"https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/yRF5c-O/breaking-news-on-screen-animated-text-graphics-news-broadcast-graphic-title-animation-loop-full-hd-1920x1080-blue-cyan_hkrmjv15e_thumbnail-1080_05.png" :imgUrl} className="card-img-top" alt="..." style={{"height" : "18rem"}}/>
                     <div className="card-body">
                         <h5 className="card-title">{title}  </h5>
                         <p className="card-text">{description}</p>
